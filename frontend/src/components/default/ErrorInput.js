4e2d3632d4af2406cb5ef0a254034ce65323caf8
shivam4198 <shivam.1234desai@gmail.com>
2022-05-24 22:14:22 -0400

    adding the codes for the components

---
+++ frontend/src/components/default/ErrorInput.js
@@ -0,0 +1,5 @@
+const ErrorInput = props => {
+    const { error, optClasses } = props;
+    return error ? <span className={`error-text ${optClasses ? optClasses : ''}`}>{error[0]}</span> : '';
+};
+export default ErrorInput;
